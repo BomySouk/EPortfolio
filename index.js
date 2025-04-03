1b3e03385dffc0ae5ae118a031e57b72fbffbf67
BomySouk <bomysouk@gmail.coc>
2025-04-02 21:01:12 -0400

    Added more animations and dark theme

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// template_ono875t 
-// service_nr05mfo 
-// SUR3M-booW5QRutt- 
+let isModalOpen = false; 
+let contrastToggle = false;
 
-// {/* <script type="text/javascript"
-//         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-// </script>
-// <script type="text/javascript">
-//    (function(){
-//       emailjs.init({
-//         publicKey: "SUR3M-booW5QRutt-",
-//       });
-//    })();
-// </script> */}
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+         document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -19,7 +17,7 @@ function contact(event) {
     const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible";
     emailjs
-        .sendform(
+        .sendForm(
             'service_nr05mfo',
             'template_ono875t',
             event.target, 
@@ -32,5 +30,16 @@ function contact(event) {
             alert(
                 "The email service is temporarily unavailable. Feel free to contact me directly bomysouk@gmail.com"
             );
-        })
+        }) 
+}
+
+
+function toggleModal() {
+    
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
 }
