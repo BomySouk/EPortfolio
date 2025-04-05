e0c44cd55c1cd02396bfc00113ddef126ea32511
BomySouk <bomysouk@gmail.coc>
2025-04-05 17:56:22 -0400

    Adjusted shapes pattern

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function moveBackground(event) {
     const y = event.clientY * scaleFactor;
 
     for (let i = 0; i < shapes.length; ++i) {
-        const isOdd = 1 % 2 !== 0;
+        const isOdd = i % 2 !== 0;
         const boolInt = isOdd ? -1 : 1;
         shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
     }
